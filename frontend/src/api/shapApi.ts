f86f919edd26d0d4be9c58461dfe34912499a551
Khwaja <khwajamoinuddin2801@gmail.com>
2026-03-01 17:06:48 +0530

    Update shapApi.ts

--- frontend/src/api/shapApi.ts
+++ frontend/src/api/shapApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL?.replace(/\/$/, "");
 
 export const getShap = (data: any) =>
   axios.post(`${API_URL}/explain`, data);
