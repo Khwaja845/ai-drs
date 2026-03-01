73bec27050c91843b7b3cd01093cb72e054af092
Khwaja <khwajamoinuddin2801@gmail.com>
2026-03-01 16:47:09 +0530

    Update predictApi.ts

--- frontend/src/api/predictApi.ts
+++ frontend/src/api/predictApi.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
-export const predictManual = (data:any)=>axios.post("http://127.0.0.1:8000/predict",data);
-export const predictFile = (file:File)=>{
-  const f=new FormData(); f.append("file",file);
-  return axios.post("http://127.0.0.1:8000/upload",f);
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const predictManual = (data: any) =>
+  axios.post(`${API_URL}/predict`, data);
+
+export const predictFile = (file: File) => {
+  const f = new FormData();
+  f.append("file", file);
+  return axios.post(`${API_URL}/upload`, f);
 };
